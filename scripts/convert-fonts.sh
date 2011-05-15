fb1a76ce735ea9366ab51d6bb40211a8411f696d
Sebastian Schubert <schubert.seb@googlemail.com>
2011-05-15 21:26:38 +0200

    convert scripts updated, are they enough?

--- scripts/convert-fonts.sh
+++ scripts/convert-fonts.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+
+font=$1
+
+case $font in
+    "MdSymbol" | "MnSymbol" )
+	echo "Chosen font family is $font"
+	;;
+    *)
+	echo "Entered font not supported"
+	exit 1
+	;;
+esac
+
 cd ..
-patch -p1 < scripts/patch_mf_sources
+rm source/* enc/*
+cp enc/$1/* enc/
+cp source/$1/* source/
+
 cd source
 
-for i in A B C D E F
+for i in A B C D E F S
 do
   for j in "" "-Bold"
   do
     for k in 5 6 7 8 9 10 12
     do
-      rm -f MnSymbol$i$j$k.[0-9]*
-      ../scripts/mf2pt1.pl MnSymbol$i$j$k --encoding=../enc/MnSymbol$i.enc
-      for k in MnSymbol$i$j$k.[0-9]*; do mv $k $k.eps;done
+      mf2pt1.pl $font$i$j$k --encoding=../enc/$font$i.enc
     done
   done
 done
@@ -22,9 +36,9 @@ for i in "" "-Bold"
 do 
   for j in 5 6 7 8 9 10 12
   do
-    ./mkMn.ff $j $i 2>&1 | grep -v showpage
-    ./postprocess.ff $j $i
-    t1disasm MnSymbol$i$j.pfb | gawk -f packsubr.awk | t1asm -b > finalpfb/MnSymbol$i$j.pfb
+    ./mkMn.ff $font $j $i # 2>&1 | grep -v showpage
+    # ./postprocess.ff $j $i
+    # t1disasm MnSymbol$i$j.pfb | gawk -f packsubr.awk | t1asm -b > finalpfb/MnSymbol$i$j.pfb
   done
 done
 
